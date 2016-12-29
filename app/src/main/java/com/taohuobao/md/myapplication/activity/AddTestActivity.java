328d56b2757e6227a902e89e663835cb7239379e
taohuobao <654394665@qq.com>
2016-12-29 10:56:16 +0800

    增加AddTest activity类  再次提交

---
+++ app/src/main/java/com/taohuobao/md/myapplication/activity/AddTestActivity.java
@@ -0,0 +1,14 @@
+package com.taohuobao.md.myapplication.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class AddTestActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
